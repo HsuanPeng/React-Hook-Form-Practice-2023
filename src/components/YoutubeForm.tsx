e5f60d7a02756e20529e3f11e7be5c3f20e12fac
lisa <lisalisa12332180@gmail.com>
2023-09-30 17:43:23 +0800

    feat: dynamic fields of useFieldArray

--- src/components/YoutubeForm.tsx
+++ src/components/YoutubeForm.tsx
@@ -1,4 +1,4 @@
-import { useForm } from "react-hook-form";
+import { useForm, useFieldArray } from "react-hook-form";
 import { DevTool } from "@hookform/devtools"; // 從畫面監看表單資料
 
 type FormValues = {
@@ -10,6 +10,9 @@ type FormValues = {
     facebook: string;
   };
   phoneNumbers: string[];
+  phNumbers: {
+    number: string;
+  }[];
 };
 
 const YoutubeForm = () => {
@@ -29,11 +32,17 @@ const YoutubeForm = () => {
           facebook: "",
         },
         phoneNumbers: ["", ""],
+        phNumbers: [{ number: "" }],
       };
     },
   });
   const { register, control, handleSubmit, formState } = form;
   const { errors } = formState;
+
+  const { fields, append, remove } = useFieldArray({
+    name: "phNumbers",
+    control,
+  });
 
   const onSubmitData = (data: FormValues) => {
     console.log("onSubmit", data);
@@ -127,6 +136,40 @@ const YoutubeForm = () => {
             id="Secondary-phone-number"
             {...register("phoneNumbers.1")}
           />
+        </div>
+
+        <div>
+          <label>List of phone numbers</label>
+          <div>
+            {fields.map((field, index) => {
+              return (
+                <div className="form-control" key={field.id}>
+                  <input
+                    type="text"
+                    {...register(`phNumbers.${index}.number`)}
+                  />
+                  {index > 0 && (
+                    <button
+                      type="button"
+                      onClick={() => {
+                        remove(index);
+                      }}
+                    >
+                      remove
+                    </button>
+                  )}
+                </div>
+              );
+            })}
+          </div>
+          <button
+            type="button"
+            onClick={() => {
+              append({ number: "" });
+            }}
+          >
+            Add phone number
+          </button>
         </div>
 
         <button>Submit</button>
