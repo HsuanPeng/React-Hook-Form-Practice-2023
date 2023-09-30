12f80a65d7b89da002b96682522ba57321c6a104
lisa <lisalisa12332180@gmail.com>
2023-09-30 13:11:49 +0800

    feat: form validation

--- src/components/YoutubeForm.tsx
+++ src/components/YoutubeForm.tsx
@@ -12,20 +12,47 @@ const YoutubeForm = () => {
   const { register, control, handleSubmit } = form;
 
   const onSubmitData = (data: FormValues) => {
-    console.log("onSubmit", data.username, data.email, data.channel);
+    console.log("onSubmit", data);
   };
 
   return (
     <div>
-      <form onSubmit={handleSubmit(onSubmitData)}>
+      <form onSubmit={handleSubmit(onSubmitData)} noValidate>
         <label htmlFor="username">Username</label>
-        <input type="text" id="username" {...register("username")} />
+        <input
+          type="text"
+          id="username"
+          {...register("username", {
+            required: {
+              value: true,
+              message: "username is required",
+            },
+          })}
+        />
 
         <label htmlFor="email">Email</label>
-        <input type="email" id="email" {...register("email")} />
+        <input
+          type="email"
+          id="email"
+          {...register("email", {
+            pattern: {
+              value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+              message: "invalid email",
+            },
+          })}
+        />
 
         <label htmlFor="channel">Channel</label>
-        <input type="text" id="channel" {...register("channel")} />
+        <input
+          type="text"
+          id="channel"
+          {...register("channel", {
+            required: {
+              value: true,
+              message: "channel is required",
+            },
+          })}
+        />
 
         <button>Submit</button>
       </form>
