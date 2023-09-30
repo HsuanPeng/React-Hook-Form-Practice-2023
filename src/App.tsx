7249b87f04d30baae83ae6b0d8d0a711fcae8f67
lisa <lisalisa12332180@gmail.com>
2023-09-30 11:04:19 +0800

    feat: manage form state

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
-import { useState } from "react";
 import YoutubeForm from "./components/YoutubeForm";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <YoutubeForm />
