4f971a5aea20568838a28929d02dc0928ed634a6
lisa <lisalisa12332180@gmail.com>
2023-09-30 11:29:06 +0800

    feat: form submission

--- src/components/YoutubeForm.tsx
+++ src/components/YoutubeForm.tsx
@@ -1,13 +1,23 @@
 import { useForm } from "react-hook-form";
 import { DevTool } from "@hookform/devtools";
 
+type FormValues = {
+  username: string;
+  email: string;
+  channel: string;
+};
+
 const YoutubeForm = () => {
-  const form = useForm();
-  const { register, control } = form;
+  const form = useForm<FormValues>();
+  const { register, control, handleSubmit } = form;
+
+  const onSubmitData = (data: FormValues) => {
+    console.log("onSubmit", data.username, data.email, data.channel);
+  };
 
   return (
     <div>
-      <form>
+      <form onSubmit={handleSubmit(onSubmitData)}>
         <label htmlFor="username">Username</label>
         <input type="text" id="username" {...register("username")} />
 
