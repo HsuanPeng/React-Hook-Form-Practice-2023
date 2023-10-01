3c460f230d8b5ccd76e4f634f5f64788dd100d4a
lisa <lisalisa12332180@gmail.com>
2023-10-01 09:39:46 +0800

    feat: reset form

--- src/components/YoutubeForm.tsx
+++ src/components/YoutubeForm.tsx
@@ -22,13 +22,16 @@ let renderCount = 0;
 
 const YoutubeForm = () => {
   renderCount++;
+
   const form = useForm<FormValues>({
     // 預設值可直接設定，或由async function取得
     defaultValues: async () => {
       const response = await fetch(
         "https://jsonplaceholder.typicode.com/users/1"
       );
+
       const data = await response.json();
+
       return {
         username: data.username,
         email: data.email,
@@ -44,6 +47,8 @@ const YoutubeForm = () => {
       };
     },
   });
+
+  // 取出方法
   const {
     register,
     control,
@@ -52,38 +57,44 @@ const YoutubeForm = () => {
     watch,
     getValues,
     setValue,
+    reset,
   } = form;
-  const { errors, touchedFields, dirtyFields, isDirty, isValid } = formState;
 
-  console.log("touchedFields", touchedFields); // 使用者是否有觸碰過該欄位
-  console.log("dirtyFields", dirtyFields); // 使用者是否有修改過該欄位
-  console.log("isDirty", isDirty); // 使用者是否有修改過表單
+  // 取出狀態
+  const {
+    errors,
+    touchedFields,
+    dirtyFields,
+    isDirty,
+    isValid,
+    isSubmitting,
+    isSubmitted,
+    isSubmitSuccessful,
+  } = formState;
+
+  // console.log("touchedFields", touchedFields); // 使用者是否有觸碰過該欄位
+  // console.log("dirtyFields", dirtyFields); // 使用者是否有修改過該欄位
+  // console.log("isDirty", isDirty); // 使用者是否有修改過表單
 
   const { fields, append, remove } = useFieldArray({
     name: "phNumbers",
     control,
   });
 
+  // 按下submit時執行
   const onSubmitData = (data: FormValues) => {
     console.log("onSubmit", data);
   };
 
   // const watchUsername = watch();
 
-  // 監聽表單資料
-  useEffect(() => {
-    const subscription = watch((value) => {
-      console.log(value);
-    });
-
-    return () => subscription.unsubscribe();
-  }, [watch]);
-
+  // 或許指定表單資料
   const handleGetValues = () => {
     console.log(getValues()); // 獲取所有表單資料
     console.log(getValues(["username", "email"])); // 獲取指定表單資料
   };
 
+  // 給予指定表單值
   const handleSetValue = () => {
     setValue("username", "Lisaaaa", {
       shouldValidate: true,
@@ -92,9 +103,26 @@ const YoutubeForm = () => {
     });
   };
 
+  // 繳交表單時有錯誤時執行
   const onError = (errors: FieldErrors<FormValues>) => {
     console.log("onError", errors);
   };
+
+  // 監聽全部表單資料
+  useEffect(() => {
+    const subscription = watch((value) => {
+      console.log(value);
+    });
+
+    return () => subscription.unsubscribe();
+  }, [watch]);
+
+  // 表單繳交成功時執行
+  useEffect(() => {
+    if (isSubmitSuccessful) {
+      reset(); // 可傳送更多行為
+    }
+  }, [isSubmitSuccessful, reset]);
 
   return (
     <div>
@@ -262,7 +290,7 @@ const YoutubeForm = () => {
           <p className="error">{errors.dob?.message}</p>
         </div>
 
-        <button disabled={!isDirty || !isValid}>Submit</button>
+        <button disabled={!isDirty || !isValid || isSubmitting}>Submit</button>
 
         <button type="button" onClick={handleGetValues}>
           getValues
@@ -270,6 +298,10 @@ const YoutubeForm = () => {
 
         <button type="button" onClick={handleSetValue}>
           setValues
+        </button>
+
+        <button type="button" onClick={() => reset()}>
+          Reset
         </button>
       </form>
       <DevTool control={control} />
